cdb61296b5f0b2e3722563aa330107bc821cd85e
DELSK <1102213456@qq.com>
2018-02-09 09:43:05 +0800

    Fifth commit20180209

--- scrapymodule/getItem.py
+++ scrapymodule/getItem.py
@@ -45,7 +45,7 @@ def get_item(itemClass):
     item["URL"] = ""
     return item
 
-def get_item1(itemClass):
+def get_item2(itemClass):
     item = itemClass()
     item["university"] = ""
     item["department"] = ""
@@ -91,4 +91,67 @@ def get_item1(itemClass):
     item["url"] = ""
     item["tegree_type"] = ""
     item["degree_level"] = 0
+    return item
+
+# 2018/02/08 修改表tmp_shool_major
+def get_item1(itemClass):
+    item = itemClass()
+    item["university"] = ""
+    item["country"] = ""
+    item["city"] = ""
+    item["website"] = ""
+    item["department"] = ""
+    item["programme"] = ""
+    item["degree_level"] = '0'
+    item["degree_type"] = ""
+    item["ucas_code"] = ""
+    item["mode"] = ""
+    item["application_date"] = ""
+    item["deadline"] = ""
+    item["start_date"] = ""
+    item["degree_description"] = ""
+    item["overview"] = ""
+    item["duration"] = ""
+    item["modules"] = ""
+    item["application_fee"] = ""
+    item["tuition_fee"] = ""
+    item["teaching"] = ""
+    item["assessment"] = ""
+    item["career"] = ""
+    item["location"] = ""
+    item["entry_requirements"] = ""
+    item["chinese_requirements"] = ""
+    item["ATAS"] = ""
+    item["GPA"] = ""
+    item["average_score"] = ""
+    item["accredited_university"] = ""
+    item["Alevel"] = ""
+    item["IB"] = ""
+    item["IELTS"] = ""
+    item["IELTS_L"] = ""
+    item["IELTS_S"] = ""
+    item["IELTS_R"] = ""
+    item["IELTS_W"] = ""
+    item["TOEFL"] = ""
+    item["TOEFL_L"] = ""
+    item["TOEFL_S"] = ""
+    item["TOEFL_R"] = ""
+    item["TOEFL_W"] = ""
+    item["GRE"] = ""
+    item["GMAT"] = ""
+    item["LSAT"] = ""
+    item["MCAT"] = ""
+    item["working_experience"] = ""
+    item["interview"] = ""
+    item["portfolio"] = ""
+    item["application_documents"] = ""
+    item["how_to_apply"] = ""
+    item["school_test"] = ""
+    item["SATI"] = ""
+    item["SATII"] = ""
+    item["SAT_code"] = ""
+    item["ACT"] = ""
+    item["ACT_code"] = ""
+    item["other"] = ""
+    item["url"] = ""
     return item
--- scrapymodule/pipelines.py
+++ scrapymodule/pipelines.py
@@ -613,3 +613,40 @@ class SchoolPipeline(InsertMysql):
                 f.write(str(e) + "\n========================")
         # self.close()
         return item
+
+# 2018/02/08 修改表tmp_shool_major
+class School1Pipeline(InsertMysql):
+    def process_item(self, item, spider):
+        sql = "insert into tmp_school_major(university, country, city, website, department, programme, degree_level, " \
+              "degree_type, ucas_code, mode, application_date, deadline, start_date, degree_description, " \
+              "overview, duration, modules, application_fee, tuition_fee, teaching, assessment, career, location, " \
+              "entry_requirements, chinese_requirements, ATAS, GPA, average_score, accredited_university, Alevel, " \
+              "IB, IELTS, IELTS_L, IELTS_S, IELTS_R, IELTS_W, TOEFL, TOEFL_L, TOEFL_S, TOEFL_R, TOEFL_W, GRE, GMAT," \
+              " LSAT, MCAT, working_experience, interview, portfolio, application_documents, how_to_apply, school_test, " \
+              "SATI, SATII, SAT_code, ACT, ACT_code, other, url) values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, " \
+              "%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, " \
+              "%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        try:
+            self.cursor.execute(sql, (item["university"], item["country"], item["city"], item["website"],
+                                      item["department"], item["programme"], item["degree_level"], item["degree_type"],
+                                      item["ucas_code"], item["mode"], item["application_date"], item["deadline"],
+                                      item["start_date"], item["degree_description"], item["overview"], item["duration"],
+                                      item["modules"], item["application_fee"], item["tuition_fee"], item["teaching"],
+                                      item["assessment"], item["career"], item["location"], item["entry_requirements"],
+                                      item["chinese_requirements"], item["ATAS"], item["GPA"], item["average_score"],
+                                      item["accredited_university"], item["Alevel"], item["IB"], item["IELTS"],
+                                      item["IELTS_L"], item["IELTS_S"], item["IELTS_R"], item["IELTS_W"], item["TOEFL"],
+                                      item["TOEFL_L"], item["TOEFL_S"], item["TOEFL_R"], item["TOEFL_W"], item["GRE"],
+                                      item["GMAT"], item["LSAT"], item["MCAT"], item["working_experience"],
+                                      item["interview"], item["portfolio"], item["application_documents"],
+                                      item["how_to_apply"], item["school_test"], item["SATI"], item["SATII"],
+                                      item["SAT_code"], item["ACT"], item["ACT_code"], item["other"], item["url"]))
+            self.db.commit()
+            print("数据插入成功")
+        except Exception as e:
+            self.db.rollback()
+            print("数据插入失败：%s" % (str(e)))
+            with open("./mysqlerror/"+item['university']+".txt", 'w', encoding="utf-8") as f:
+                f.write(str(e) + "\n========================")
+        # self.close()
+        return item
